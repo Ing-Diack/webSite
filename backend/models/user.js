2d34313277c96425c6608c16f9b4362213c77bca
Diack <diacksidibe500@gmail.com>
2023-03-08 16:11:45 +0100

    suppression de mongoose validator

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-const mongooseValidator = require('mongoose-unique-validator')
 
 const userSchema = mongoose.Schema({
     username:{type:String, required :true},
     email:{type:String,required:true,unique:true},
     password : {type:String,required:true}
 });
-userSchema.plugin(mongooseValidator);
+
 
 module.exports = mongoose.model('User', userSchema);
